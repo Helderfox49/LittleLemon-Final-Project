ea0cfda1b06bc4921908eecc3f96d745f5c1e656
Helderfox49 <mbottoguyzo2012@gmail.com>
2025-08-06 00:55:39 +0100

    Implementing User authentication with Djoser

--- Littlemon/settings.py
+++ Littlemon/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Personnal App
+    'LittlemonAPI',
+
+    # Thirds Packages
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +128,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
