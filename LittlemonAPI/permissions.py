c3a5706e1c655b12d7a80592cc2a2c55cdc4ee92
Helderfox49 <mbottoguyzo2012@gmail.com>
2025-08-06 10:12:53 +0100

    Implementing Endpoints for managing user groups

---
+++ LittlemonAPI/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsManager(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.groups.filter(name="Manager").exists()
+    
+
+class IsDelivery(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.groups.filter(name="Delivery Crew").exists()
